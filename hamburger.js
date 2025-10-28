3357245c925bf8e48c3716ca249bbd16a862416d
SudhinRj <rjsudhin@users.noreply.github.com>
2025-10-28 14:24:15 +0530

    hamburger menu line to x is configured

--- hamburger.js
+++ hamburger.js
@@ -1,9 +1,17 @@
 // hamburger configure
 const hamburger = document.querySelector('.hamburger')
 const ulMenu = document.querySelector('.list-menu')
+const lines = hamburger.querySelectorAll('span')
 
 
 hamburger.addEventListener('click', (e) => {
+  // showing the menu list element
   ulMenu.classList.toggle('active')
+
+  // change the lines to x adding class 
+  lines.forEach(line => {
+    line.classList.toggle('change')
+    console.log(line)
+  })
   console.log(ulMenu)
 })
